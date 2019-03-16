e12684bcd00aa4286ee95c90932958dac02d2e8c
Kobie Arndt <arndtkobie@gmail.com>
2019-03-15 23:42:08 -0400

    Scroll function done

--- ImagineVisualizer.ino
+++ ImagineVisualizer.ino
@@ -87,17 +87,24 @@ void setup(){
 /**
  * This function simulates the scroll.  What this does is
  * takes in the int pos and generates a color based 
- * on virtual wheel.
+ * on virtual wheel.  
  */
  CRGB Scroll(int pos){
   CRGB color (0,0,0);
-  if(pos < 85){
-    color.g = 0;
-    color.r = ((float)pos / 85.0f) * 255.0f;
+  if(pos < 85){ //This checks to see if the rgb scale is below 85
+    color.g = 0; //This will set it to 0
+    color.r = ((float)pos / 85.0f) * 255.0f; 
     color.b = 255 - color.r;
   }else if (pos < 170){
     color.g = ((float)(pos - 85) / 85.0f) * 255.0f;
+    color.r = 255 - color.g;
+    color.b = 0;
+  }else if(pos < 256){
+    color.b = ((float)(pos - 170) / 85.0f) * 255.0f;
+    color.g = 255 - color.b;
+    color.r = 1;
   }
+  return color;
  }
 
 
